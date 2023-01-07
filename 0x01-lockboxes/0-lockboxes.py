e427bf1dc943034cbf1e3455da84df4fe6ce7543
LouisGichiah <louiegpaul@gmail.com>
2023-01-07 17:46:26 +0300

    add usr/bin/python3 line

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,15 +1,17 @@
+#!/usr/bin/python3
+'''LockBoxes Challenge'''
 def canUnlockAll(boxes):
-    # Step 1
+    '''Step 1 '''
     unopened = set(range(1, len(boxes)))
-    # Step 2
+    ''' Step 2 '''
     keys = boxes[0]
-    # Step 3
+    '''Step 3 '''
     while keys:
-        # Step 3a
+        '''Step 3a'''
         key = keys.pop(0)
-        # Step 3b
+        '''Step 3b'''
         if key in unopened:
             unopened.remove(key)
             keys.extend(boxes[key])
-    # Step 4
+    '''Step 4'''
     return not unopened
